8189d9ab6b1cd77530c22f103e4fd2d12af7c33e
Sivasangari28 <158203603+Sivasangari28@users.noreply.github.com>
2024-10-01 13:52:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+let m=prompt("enter the first number")
+let n=prompt("enter the second number")
+if(m>n){
+    alert("zero")
+}
+else{
+    let s=0
+    let i
+    for(i=m;i<=n;i++)
+        sum=i*i*i;
+    alert(sum)
+}
